04281e90627c9c55084495416c78113c8bd2bf60
hernan <hernan97carp@hotmail.com>
2024-07-17 17:17:57 -0300

    first commit

---
+++ Python_Fundamentals/Basic/0_helloworld.py
@@ -0,0 +1,33 @@
+print("Hello world")
+
+my_boolean = True
+my_int = 2
+my_float = 2.5
+
+print(f"Hello, this is a boolean which is {my_boolean}. The value of the int is {my_int} and the value of the float is {my_float}!")
+
+# Introduction
+# Day 1 - 30DaysOfPython Challenge
+
+print(2 + 3)   # addition(+)
+print(3 - 1)   # subtraction(-)
+print(2 * 3)   # multiplication(*)
+print(3 / 2)   # division(/)
+print(3 ** 2)  # exponential(**)
+print(3 % 2)   # modulus(%)
+print(3 // 2)  # Floor division operator(//)
+
+# Checking data types
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('hsd'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'hsd':'hsd'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+
+
+
